73f2f1a91f4e9415afe6d6154b0558164666661c
yvesmarieplard <72050136+yvesmarieplard@users.noreply.github.com>
2020-10-29 00:57:06 +0100

    updated footers

--- mail.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $retour = mail('ymplard@gmail.com', 'Sent from contact section of personal portfolio', $_POST['message'], 'From :'. $_POST['email']);
-    if ($retour) {
-        echo '<p>Your message was sent.</p>';
-    }
-?>
